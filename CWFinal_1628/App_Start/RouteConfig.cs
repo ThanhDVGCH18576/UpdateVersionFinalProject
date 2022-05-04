3f681a2c481dafda697342e0c1737597dee47c82
DuyThanh <65640865+ThanhDVGCH18576@users.noreply.github.com>
2022-05-04 17:31:53 +0700

    some changes

--- CWFinal_1628/App_Start/RouteConfig.cs
+++ CWFinal_1628/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CWFinal_1628
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
